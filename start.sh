c35b2ae7ce7acc94837ec8d11049d1713a52f76a
lavanya1397 <lavanya1397@gmail.com>
2026-03-25 17:24:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -e  # stop on error
+
 echo "PORT is: $PORT"
 echo "Starting app..."
 
-python -m uvicorn src.api:app --host 0.0.0.0 --port ${PORT:-10000}
+python -m uvicorn src.api:app --host 0.0.0.0 --port ${PORT:-10000} --log-level debug
